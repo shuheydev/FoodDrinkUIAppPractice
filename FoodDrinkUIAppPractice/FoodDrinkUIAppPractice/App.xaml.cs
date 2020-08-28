e11db683e35c2fe600772e9dbf1da68e9c272331
shuheydev <shuhey.dev@gmail.com>
2020-08-28 14:41:22 +0900

    Add project files.

---
+++ FoodDrinkUIAppPractice/FoodDrinkUIAppPractice/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FoodDrinkUIAppPractice.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodDrinkUIAppPractice
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MenuDetailPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
